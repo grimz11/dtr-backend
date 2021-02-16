60d50a73cbcfd3f4370d544856f31ae6c3bfaea7
Mark Andog <reymark@hyperstacksinc.com>
2021-02-16 14:34:46 +0800

    Changed db to postgres

---
+++ config/database.js
@@ -0,0 +1,47 @@
+const parse = require('pg-connection-string');
+
+module.exports = ({ env }) => {
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL);
+
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.username,
+            password: config.password,
+            ssl: true
+          },
+          // options: {
+          //   ssl: false
+          // },
+        },
+      },
+    }
+  }
+  
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "mysql",
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 3306),
+          database: env("DATABASE_NAME", "dtr"),
+          username: env("DATABASE_USERNAME", "hyperstacksinc"),
+          password: env("DATABASE_PASSWORD", "Hello101!"),
+          ssl: env.bool("DATABASE_SSL", true),
+        },
+        options: {},
+      },
+    },
+  };
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0fb7546f-310d-4acb-98a1-8293b6a0eeb3'
+};
