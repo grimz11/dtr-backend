b08e4a1cb0b48e117ba697c0bdd244876a89d59b
Mark <reymark@hyperstacksinc.com>
2021-02-16 23:49:44 +0800

    Removed config/prod

--- config/database.js
+++ config/database.js
@@ -1,18 +1,41 @@
 module.exports = ({ env }) => ({
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "mysql",
-          host: env("DATABASE_HOST", "127.0.0.1"),
-          port: env.int("DATABASE_PORT", 3306),
-          database: env("DATABASE_NAME", "dtr"),
-          username: env("DATABASE_USERNAME", "hyperstacksinc"),
-          password: env("DATABASE_PASSWORD", "Hello101!"),
-          ssl: env.bool("DATABASE_SSL", true),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
         },
-        options: {},
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
+  },
 });
+// module.exports = ({ env }) => ({
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "bookshelf",
+//         settings: {
+//           client: "mysql",
+//           host: env("DATABASE_HOST", "127.0.0.1"),
+//           port: env.int("DATABASE_PORT", 3306),
+//           database: env("DATABASE_NAME", "dtr"),
+//           username: env("DATABASE_USERNAME", "hyperstacksinc"),
+//           password: env("DATABASE_PASSWORD", "Hello101!"),
+//           ssl: env.bool("DATABASE_SSL", true),
+//         },
+//         options: {},
+//       },
+//     },
+// });
