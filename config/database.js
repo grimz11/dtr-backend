a3dc46bfeec07a91d3bc17012fd3752dba74306b
Mark Andog <reymark@hyperstacksinc.com>
2021-02-16 16:11:42 +0800

    Change dynamic port

--- config/database.js
+++ config/database.js
@@ -24,22 +24,22 @@ module.exports = ({ env }) => {
     }
   }
   
-  // return {
-  //   defaultConnection: "default",
-  //   connections: {
-  //     default: {
-  //       connector: "bookshelf",
-  //       settings: {
-  //         client: "mysql",
-  //         host: env("DATABASE_HOST", "127.0.0.1"),
-  //         port: env.int("DATABASE_PORT", 3306),
-  //         database: env("DATABASE_NAME", "dtr"),
-  //         username: env("DATABASE_USERNAME", "hyperstacksinc"),
-  //         password: env("DATABASE_PASSWORD", "Hello101!"),
-  //         ssl: env.bool("DATABASE_SSL", true),
-  //       },
-  //       options: {},
-  //     },
-  //   },
-  // };
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "mysql",
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 3306),
+          database: env("DATABASE_NAME", "dtr"),
+          username: env("DATABASE_USERNAME", "hyperstacksinc"),
+          password: env("DATABASE_PASSWORD", "Hello101!"),
+          ssl: env.bool("DATABASE_SSL", true),
+        },
+        options: {},
+      },
+    },
+  };
 };
--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: process.env.PORT || env.int('PORT'),
+  port: process.env.PORT || 1337,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '54c971298849c7ab4c1b2a4eede84703'),
