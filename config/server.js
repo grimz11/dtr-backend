4b81f63165eada40002c7778ed0021fcd4761ef4
Mark <reymark@hyperstacksinc.com>
2021-02-16 23:56:17 +0800

    Add admin url

--- config/server.js
+++ config/server.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: 'https://hyperstack-dtr-backend.herokuapp.com',
   admin: {
-    url: '/',
-    serverAdminPanel: false,
-    // auth: {
-    //   secret: env('ADMIN_JWT_SECRET', '54c971298849c7ab4c1b2a4eede84703'),
-    // },
+    url: '/admin',
+    // serverAdminPanel: false,
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54c971298849c7ab4c1b2a4eede84703'),
+    },
   },
 });
