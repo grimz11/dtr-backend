6afe0c6e9e92a9e4e67458aaa92d1aec8e869cef
Mark Andog <reymark@hyperstacksinc.com>
2021-02-18 13:13:14 +0800

    Media change maxage

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => {
+  (function runForever() {
+    setTimeout(runForever, 200000);
+    return 300000;
+  })()
+
   return {
     upload: {
       providerOptions: {
         localServer: {
-          maxage: (function runForever() {
-            setTimeout(runForever, 200000);
-            return 300000;
-          })(),
+          maxage: 300000000000,
         },
       },
     },
