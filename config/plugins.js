47a15889aafa8804fb4d94918f9c826934b0bd76
Mark Andog <reymark@hyperstacksinc.com>
2021-02-18 10:04:41 +0800

    Upload maxAge set as a loop function

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,14 @@
-module.exports = ({ env })=> {
-  let age = 300000;
-  (function runForever(){
-    // Do something here
-    age = 300000;
-    setTimeout(runForever, 200000)
-  })()
-
+module.exports = ({ env }) => {
   return {
-  upload: {
-    providerOptions: {
-      localServer: {
-        maxage: age
-      }
-    }
-  }
-}};
+    upload: {
+      providerOptions: {
+        localServer: {
+          maxage: (function runForever() {
+            setTimeout(runForever, 200000);
+            return 300000;
+          })(),
+        },
+      },
+    },
+  };
+};
