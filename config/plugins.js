2ecf4c038545bbff90c9ec8079fe44f302b06d67
Mark Andog <reymark@hyperstacksinc.com>
2021-02-19 15:40:05 +0800

    Change upload to cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,16 @@
 module.exports = ({ env }) => {
-  (function runForever() {
-    setTimeout(runForever, 200000);
-    return 300000;
-  })()
-
+  // (function runForever() {
+  //   setTimeout(runForever, 200000);
+  //   return 300000;
+  // })()
+ 
   return {
     upload: {
+      provider: 'cloudinary',
       providerOptions: {
-        localServer: {
-          maxAge: 300000000000,
-        },
+        cloud_name: env("CLOUD_NAME"),
+        api_key: env('API_KEY'),
+        api_secret: env('API_SECRET'),
       },
     },
   };
