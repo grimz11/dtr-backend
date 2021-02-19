2e059fb5b8fbf63453fdca14fadb64755b9aacb8
Mark Andog <reymark@hyperstacksinc.com>
2021-02-19 15:47:35 +0800

    Remove cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -4,14 +4,14 @@ module.exports = ({ env }) => {
   //   return 300000;
   // })()
  
-  return {
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env("CLOUD_NAME"),
-        api_key: env('API_KEY'),
-        api_secret: env('API_SECRET'),
-      },
-    },
-  };
+  // return {
+  //   upload: {
+  //     provider: 'cloudinary',
+  //     providerOptions: {
+  //       cloud_name: env("CLOUD_NAME"),
+  //       api_key: env('API_KEY'),
+  //       api_secret: env('API_SECRET'),
+  //     },
+  //   },
+  // };
 };
